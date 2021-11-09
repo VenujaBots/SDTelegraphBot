1cd6fb2f19f61e57e477ee4260dcb45ff200d259
Venuja Thilakarathna <91406478+VenujaBots@users.noreply.github.com>
2021-11-09 17:38:42 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "2121334565:AAGM5LvwIBk1nnWDYwu4aes0Sa7HaWAeIiY")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", 7395896))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "cd3998ddf318dad74d7c506731bc0abc")
